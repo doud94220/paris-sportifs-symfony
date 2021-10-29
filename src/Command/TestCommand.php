abb359aad006cfbf7a06b03decb62c5e66854dfd
Edouard ANTHONY <doud75@gmail.com>
2021-10-29 17:32:35 +0200

    test commande symfony

---
+++ src/Command/TestCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\MailerInterface;
+
+class TestCommand extends Command
+{
+    protected static $defaultName = 'test';
+    protected $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        parent::__construct(null);
+        $this->mailer = $mailer;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('ma commande de test');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = new TemplatedEmail();
+
+        $email->from(new Address("doud75@gmail.fr", "Confirmation Account Creation"))
+            ->to('doud-test@outlook.fr')
+            ->htmlTemplate("/emails/testCommand.html.twig")
+            ->subject("Test Command Symfony");
+        $this->mailer->send($email);
+
+        return Command::SUCCESS;
+    }
+}
