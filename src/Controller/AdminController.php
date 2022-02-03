76c83d6289318e3bc44fd2305d58e9e4dd55f219
Edouard ANTHONY <doud75@gmail.com>
2022-02-03 10:40:21 +0100

    modif en carton pour retester deploiement sur heroku

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -21,6 +21,7 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+//commentaire en carton
 class AdminController extends AbstractController
 {
     /**
