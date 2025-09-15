bae96e56839c7add73c6a5529eb2cffead155596
doud94220 <edouard.anthony@gmail.com>
2025-09-15 15:19:11 +0200

    lalala

--- tests/LOCAL-PourJenkis/S1.js
+++ tests/LOCAL-PourJenkis/S1.js
@@ -9,21 +9,33 @@ const { runTest4 } = require('./4.ConnectionAsClassikUser.js');
 const { runTest7_2 } = require('./7._2_ConsultOneTennisPlayerInformation.js');
 const { runTest47 } = require('./47.ClassikUserLogout.js');
 
-async function main() {
-    let driver = await new Builder().forBrowser('chrome').usingServer(serverUrl).build();
+let driver;
 
-    try {
-        await runTest2(driver);
-        await runTest4(driver);
-        await runTest7_2(driver);
-        await runTest47(driver);
+describe('S1', function () {
+    this.timeout(20000); // Set a global timeout for the suite
 
-    } catch (error) {
-        console.log("Erreur - Catch - S1 has a problem...");
-        console.error('Test failed. Error details:', error);
-    } finally {
+    before(async function main() {
+        driver = await new Builder().forBrowser('chrome').usingServer(serverUrl).build();
+
+        it('should successfully register a new user', async function () {
+            await runTest2(driver);
+        });
+
+        it('should successfully register a new user', async function () {
+            await runTest4(driver);
+        });
+
+        it('should successfully register a new user', async function () {
+            await runTest7_2(driver);
+        });
+
+        it('should successfully register a new user', async function () {
+            await runTest47(driver);
+        });
+    });
+
+    after(async function () {
+        // This runs after all tests and cleans up the driver
         await driver.quit();
-    }
-}
-
-main();
+    });
+});
