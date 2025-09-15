c546117a68c96f1bc2533f94eb8ab9d331c13af1
doud94220 <edouard.anthony@gmail.com>
2025-09-15 15:49:09 +0200

    virer les tr/catch partout

--- tests/LOCAL-PourJenkis/2.RegisterAsClassikUser.js
+++ tests/LOCAL-PourJenkis/2.RegisterAsClassikUser.js
@@ -5,48 +5,41 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 async function runTest2(driver) {
     // let driver = await new Builder().forBrowser('chrome').build();
 
-    try {
-        await driver.get('http://127.0.0.1:8000/');
-        console.log("1");
+    await driver.get('http://127.0.0.1:8000/');
+    console.log("1");
 
-        const InscriptionLink = await driver.wait(until.elementLocated(By.css('ul > li:nth-child(2) > a.nav-link')), 3000);
-        console.log("2");
+    const InscriptionLink = await driver.wait(until.elementLocated(By.css('ul > li:nth-child(2) > a.nav-link')), 3000);
+    console.log("2");
 
-        await InscriptionLink.click();
-        console.log("3");
+    await InscriptionLink.click();
+    console.log("3");
 
-        const creationButton = await driver.wait(until.elementLocated(By.css('button.btn-success')), 3000);
-        //Pour Info : Failed to execute 'querySelector' on 'Document': 'button.btn-success:has-text("Création")' is not a valid selector
-        console.log("4");
+    const creationButton = await driver.wait(until.elementLocated(By.css('button.btn-success')), 3000);
+    //Pour Info : Failed to execute 'querySelector' on 'Document': 'button.btn-success:has-text("Création")' is not a valid selector
+    console.log("4");
 
-        await driver.findElement(By.id('user_firstName')).sendKeys('Eric', Key.RETURN);
-        console.log("5");
+    await driver.findElement(By.id('user_firstName')).sendKeys('Eric', Key.RETURN);
+    console.log("5");
 
-        await driver.findElement(By.id('user_lastName')).sendKeys('DUGNOU', Key.RETURN);
-        console.log("6");
+    await driver.findElement(By.id('user_lastName')).sendKeys('DUGNOU', Key.RETURN);
+    console.log("6");
 
-        await driver.findElement(By.id('user_nickName')).sendKeys('Ricou', Key.RETURN);
-        console.log("7");
+    await driver.findElement(By.id('user_nickName')).sendKeys('Ricou', Key.RETURN);
+    console.log("7");
 
-        await driver.findElement(By.id('user_email')).sendKeys('eric.dujnou@gmx.fr', Key.RETURN);
-        console.log("8");
+    await driver.findElement(By.id('user_email')).sendKeys('eric.dujnou@gmx.fr', Key.RETURN);
+    console.log("8");
 
-        await driver.findElement(By.id('user_password')).sendKeys('Ricou', Key.RETURN);
-        console.log("9");
+    await driver.findElement(By.id('user_password')).sendKeys('Ricou', Key.RETURN);
+    console.log("9");
 
-        // ----- Pas besoin, Selenium valide le form (équivaut click bouton) quand tous les champs sont remplis
-        // await creationButton.click();
-        // console.log("10");
+    // ----- Pas besoin, Selenium valide le form (équivaut click bouton) quand tous les champs sont remplis
+    // await creationButton.click();
+    // console.log("10");
 
-        const successRegistrationMsg = await driver.wait(until.elementLocated(By.css('div.alert-success > p')), 3000);
-        console.log("Classik user is registered !");
-    } catch (error) {
-        console.log("Erreur - Catch - Fichier '2.RegisterAsClassikUser.js'");
-        console.error('Test failed. Error details:', error);
-    } finally {
-        // await driver.quit();
-    }
-}
+    const successRegistrationMsg = await driver.wait(until.elementLocated(By.css('div.alert-success > p')), 3000);
+    console.log("Classik user is registered !");
+};
 
 module.exports = { runTest2 };
 
--- tests/LOCAL-PourJenkis/7._2_ConsultOneTennisPlayerInformation.js
+++ tests/LOCAL-PourJenkis/7._2_ConsultOneTennisPlayerInformation.js
@@ -6,60 +6,53 @@ const { strictEqual } = require('assert');
 async function runTest7_2(driver) {
     // let driver = await new Builder().forBrowser('chrome').build();
 
-    try {
-        //Accéder à la page du joueur Federer
-        await driver.get('http://127.0.0.1:8000/tennis-players/');
-        const playerRogerFederer = await driver.findElement(By.css('.color-players:nth-child(5) > b'));
-        await playerRogerFederer.click();
-        console.log("0");
+    //Accéder à la page du joueur Federer
+    await driver.get('http://127.0.0.1:8000/tennis-players/');
+    const playerRogerFederer = await driver.findElement(By.css('.color-players:nth-child(5) > b'));
+    await playerRogerFederer.click();
+    console.log("0");
 
-        //Vérifier les informations du joueur
-        const playerNameElement = await driver.findElement(By.css('div.card-body > h5'));
-        const playerName = await playerNameElement.getText();
-        strictEqual(playerName, 'Roger FEDERER', 'Le NOM du joueur est incorrect...');
-        console.log("1");
+    //Vérifier les informations du joueur
+    const playerNameElement = await driver.findElement(By.css('div.card-body > h5'));
+    const playerName = await playerNameElement.getText();
+    strictEqual(playerName, 'Roger FEDERER', 'Le NOM du joueur est incorrect...');
+    console.log("1");
 
-        const playerCountryElement = await driver.findElement(By.css('div.card-body > h6'));
-        const playerCountry = await playerCountryElement.getText();
-        strictEqual(playerCountry, 'Switzerland', 'Le PAYS du joueur est incorrect...');
-        console.log("2");
+    const playerCountryElement = await driver.findElement(By.css('div.card-body > h6'));
+    const playerCountry = await playerCountryElement.getText();
+    strictEqual(playerCountry, 'Switzerland', 'Le PAYS du joueur est incorrect...');
+    console.log("2");
 
-        const playerCountryFlagElement = await driver.findElement(By.css('div.card-body > img'));
-        const playerCountryFlagPath = await playerCountryFlagElement.getAttribute('src');
+    const playerCountryFlagElement = await driver.findElement(By.css('div.card-body > img'));
+    const playerCountryFlagPath = await playerCountryFlagElement.getAttribute('src');
 
-        if (playerCountryFlagPath.includes('/img/Swiss.svg')) {
-            console.log("3 - Correct Path !");
-        }
-        else {
-            console.log("3 - Incorrect Path....");
-        };
-
-        const playerPictureElement = await driver.findElement(By.css('div.card > img'));
-        const playerPictureElementPath = await playerPictureElement.getAttribute('src');
-
-        if (playerPictureElementPath.includes('/img/Federer.png')) {
-            console.log("4 - Correct Path !");
-        }
-        else {
-            console.log("4- Incorrect Path....");
-        };
-
-        const playerAgeElement = await driver.findElement(By.css('div.card > ul > li:nth-child(1)'));
-        const playerAge = await playerAgeElement.getText();
-        strictEqual(playerAge, 'Age : 40', 'Incorrect player age');
-        console.log("5");
-
-        const playerRankingElement = await driver.findElement(By.css('div.card > ul > li:nth-child(2)'));
-        const playerRanking = await playerRankingElement.getText();
-        strictEqual(playerRanking, 'Atp Ranking : 9', 'Incorrect ranking');
-        console.log("6 - We have just consulted one player information !");
-    } catch (error) {
-        console.log("Erreur - Catch - 7._2_ConsultOneTennisPlayerInformation.js");
-        console.error('Test failed. Error details:', error);
-    } finally {
-        //await driver.quit();
+    if (playerCountryFlagPath.includes('/img/Swiss.svg')) {
+        console.log("3 - Correct Path !");
     }
-}
+    else {
+        console.log("3 - Incorrect Path....");
+    };
+
+    const playerPictureElement = await driver.findElement(By.css('div.card > img'));
+    const playerPictureElementPath = await playerPictureElement.getAttribute('src');
+
+    if (playerPictureElementPath.includes('/img/Federer.png')) {
+        console.log("4 - Correct Path !");
+    }
+    else {
+        console.log("4- Incorrect Path....");
+    };
+
+    const playerAgeElement = await driver.findElement(By.css('div.card > ul > li:nth-child(1)'));
+    const playerAge = await playerAgeElement.getText();
+    strictEqual(playerAge, 'Age : 40', 'Incorrect player age');
+    console.log("5");
+
+    const playerRankingElement = await driver.findElement(By.css('div.card > ul > li:nth-child(2)'));
+    const playerRanking = await playerRankingElement.getText();
+    strictEqual(playerRanking, 'Atp Ranking : 9', 'Incorrect ranking');
+    console.log("6 - We have just consulted one player information !");
+};
 
 module.exports = { runTest7_2 };
 
