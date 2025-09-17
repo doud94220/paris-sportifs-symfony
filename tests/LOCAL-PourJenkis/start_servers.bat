04fc7349eb65a026fe54c4a7abc3efba3f6abbf0
doud94220 <edouard.anthony@gmail.com>
2025-09-17 11:37:52 +0200

    cmd /c dans le start_servers.bat + powershell + correctif3

--- tests/LOCAL-PourJenkis/start_servers.bat
+++ tests/LOCAL-PourJenkis/start_servers.bat
@@ -1,2 +1,2 @@
 powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Process java -ArgumentList '-jar', 'C:\SeleniumServerGrid\selenium-server-4.35.0.jar', 'standalone'"
-powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Process php -ArgumentList 'bin/console', 'serve'"
+powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Process php -ArgumentList 'C:\ProgramData\Jenkins\.jenkins\workspace\JobDoud1\bin\console', 'serve'"
