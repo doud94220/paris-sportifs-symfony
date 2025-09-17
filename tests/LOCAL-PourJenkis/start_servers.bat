a8adf204d86551bdc4a93886746e06be2dce870a
doud94220 <edouard.anthony@gmail.com>
2025-09-17 11:28:44 +0200

    cmd /c dans le start_servers.bat + powershell + correctif

--- tests/LOCAL-PourJenkis/start_servers.bat
+++ tests/LOCAL-PourJenkis/start_servers.bat
@@ -1,2 +1,2 @@
 powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Process java -ArgumentList '-jar', 'C:\SeleniumServerGrid\selenium-server-4.35.0.jar', 'standalone'"
-powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Process 'C:\ProgramData\Jenkins\.jenkins\workspace\JobDoud1\symfony.exe' -ArgumentList 'serve'"
+powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Process symfony -ArgumentList 'serve'"
