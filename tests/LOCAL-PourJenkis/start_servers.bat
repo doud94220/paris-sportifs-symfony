2fb46f65a7d3d144d666eb0dc6582c9892ccc2f5
doud94220 <edouard.anthony@gmail.com>
2025-09-17 10:51:52 +0200

    cmd /c dans le start_servers.bat

--- tests/LOCAL-PourJenkis/start_servers.bat
+++ tests/LOCAL-PourJenkis/start_servers.bat
@@ -1,2 +1,2 @@
-start "" "C:\SeleniumServerGrid\selenium-server-4.35.0.jar" standalone
-start "" "C:\ProgramData\Jenkins\.jenkins\workspace\JobDoud1\symfony.exe" serve
+cmd /c start "" "C:\SeleniumServerGrid\selenium-server-4.35.0.jar" standalone
+cmd /c start "" "C:\ProgramData\Jenkins\.jenkins\workspace\JobDoud1\symfony.exe" serve
