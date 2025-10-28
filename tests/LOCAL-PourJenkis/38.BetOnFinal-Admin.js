6aefe40364fdc44961465fdc8fb7d5a3b8c76163
doud94220 <edouard.anthony@gmail.com>
2025-10-28 12:49:20 +0100

    Build 308*

--- tests/LOCAL-PourJenkis/38.BetOnFinal-Admin.js
+++ tests/LOCAL-PourJenkis/38.BetOnFinal-Admin.js
@@ -1,84 +1,51 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const { strictEqual } = require('assert');
 
-// require('chromedriver');
+async function runTest38(driver, BASE_URL) {
 
-async function runTest38(driver) {
-    // let driver = await new Builder().forBrowser('chrome').build();
+    //Accéder à la page du pari de la finale
+    // const URL_BET_FINAL = 'http://127.0.0.1:8000/lets_bet/final';
+    const URL_BET_FINAL = `${BASE_URL}/lets_bet/final`;
+    await driver.get(URL_BET_FINAL);
+    console.log("7 - Navigation forcée vers la page de pari de la finale");
 
-    try {
+    //Bet on final
+    await driver.wait(until.elementLocated(By.name('winnerBet_1')), 3000);
+    console.log("8");
+    const listPlayerOnelement = await driver.findElement(By.name('winnerBet_1'));
+    console.log("9");
+    const optionElement = await listPlayerOnelement.findElement(By.xpath("./option[@value='1']"));
+    console.log("10");
+    await optionElement.click();
+    console.log("11 - L'option 'Novak DJOKOVIC' a été sélectionnée avec succès.");
+    const listSetNumberElement = await driver.findElement(By.name('numberSetsBet_1'));
+    console.log("12");
+    const optionSetNumberElement = await listSetNumberElement.findElement(By.xpath("./option[@value='4']"));
+    console.log("13");
+    await optionSetNumberElement.click();
+    console.log("14 - Le nombre de sets a été sélectionné");
 
-        // -------------------------------------------- Connection as ADMIN --------------------------------------------
-        // await driver.get('http://127.0.0.1:8000/');
-        // console.log("1");
-
-        // const connectionButton = await driver.wait(until.elementLocated(By.css('ul > li:nth-child(3) > a')), 3000);
-        // console.log("2");
-
-        // await connectionButton.click();
-        // console.log("3");
-
-        // await driver.findElement(By.id('login_email')).sendKeys('doud75@gmail.com', Key.RETURN);
-        // console.log("4");
-
-        // await driver.findElement(By.id('login_password')).sendKeys('Doud', Key.RETURN);
-        // console.log("5");
-
-        // await driver.wait(until.elementLocated(By.css('li > a.btn-danger')), 3000);
-        // console.log("6 - On est loggué en admin !");
-        // -------------------------------------------------------------------------------------------------------------
-
-        //Accéder à la page du pari de la finale
-        const URL_BET_FINAL = 'http://127.0.0.1:8000/lets_bet/final';
-        await driver.get(URL_BET_FINAL);
-        console.log("7 - Navigation forcée vers la page de pari de la finale");
-
-        //Bet on final
-        await driver.wait(until.elementLocated(By.name('winnerBet_1')), 3000);
-        console.log("8");
-        const listPlayerOnelement = await driver.findElement(By.name('winnerBet_1'));
-        console.log("9");
-        const optionElement = await listPlayerOnelement.findElement(By.xpath("./option[@value='1']"));
-        console.log("10");
-        await optionElement.click();
-        console.log("11 - L'option 'Novak DJOKOVIC' a été sélectionnée avec succès.");
-        const listSetNumberElement = await driver.findElement(By.name('numberSetsBet_1'));
-        console.log("12");
-        const optionSetNumberElement = await listSetNumberElement.findElement(By.xpath("./option[@value='4']"));
-        console.log("13");
-        await optionSetNumberElement.click();
-        console.log("14 - Le nombre de sets a été sélectionné");
-
-        //Validate final bet
-        async function elementToBeClickable(driver, locator, timeout = 3000) {
-            const element = await driver.wait(until.elementLocated(locator), timeout);
-            await driver.wait(until.elementIsVisible(element), timeout);
-            await driver.wait(until.elementIsEnabled(element), timeout);
-            // petit délai pour être sûr que rien ne le recouvre
-            await driver.sleep(200);
-            return element;
-        }
-        console.log("15");
-        const validateBetsButton = await elementToBeClickable(driver, By.id('button-text'));
-        console.log("16");
-        await validateBetsButton.click();
-        console.log("17");
-
-        //Verify success msg
-        const successBetsRegistration = await driver.wait(until.elementLocated(By.css('div.alert-success > p')), 3000);
-        const successMsg_registration = await successBetsRegistration.getText();
-        console.log(`18 - Success message : "${successMsg_registration}"`);
-        strictEqual(successMsg_registration, 'Vos paris ont bien été enregistrés', 'Le message de succès ne correspond pas...');
-        console.log("19 - Admin Final Bet Registered !");
+    //Validate final bet
+    async function elementToBeClickable(driver, locator, timeout = 3000) {
+        const element = await driver.wait(until.elementLocated(locator), timeout);
+        await driver.wait(until.elementIsVisible(element), timeout);
+        await driver.wait(until.elementIsEnabled(element), timeout);
+        // petit délai pour être sûr que rien ne le recouvre
+        await driver.sleep(200);
+        return element;
     }
-    catch (error) {
-        console.log("Erreur - Catch");
-        console.error('Test failed. Error details:', error);
-    } finally {
-        //await driver.quit();
-    }
+    console.log("15");
+    const validateBetsButton = await elementToBeClickable(driver, By.id('button-text'));
+    console.log("16");
+    await validateBetsButton.click();
+    console.log("17");
+
+    //Verify success msg
+    const successBetsRegistration = await driver.wait(until.elementLocated(By.css('div.alert-success > p')), 3000);
+    const successMsg_registration = await successBetsRegistration.getText();
+    console.log(`18 - Success message : "${successMsg_registration}"`);
+    strictEqual(successMsg_registration, 'Vos paris ont bien été enregistrés', 'Le message de succès ne correspond pas...');
+    console.log("19 - Admin Final Bet Registered !");
 }
-
-// runTest38();
 
 module.exports = { runTest38 };
